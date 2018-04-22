1c2dc66fad4bf9f633c0d80de41f0f7992302783
Asger <s163933@student.dtu.dk>
2018-04-22 14:57:09 +0200

    Inconsistent naming

--- ECU2018/ECU2018/ingAndinjArray.cpp
+++ ECU2018/ECU2018/ingAndinjArray.cpp
@@ -10,7 +10,7 @@ uint32_t ignitionArray[25] = {
 uint32_t injectionArray[25] = {
 	0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 //sample values, choosen based on advanced calculations
 };
-INTERPOL Interpolation_map(double rpm) {
+INTERPOL interpolation_map(double rpm) {
 	double RPM_DIVIDED = rpm / 250;
 	INTERPOL INTERPOLATION;
 	for (int i = 0; i <= 25; i++) {

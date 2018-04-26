9d75b846e6c32e149872ffb20ef6cd18dd8b3c80
Frupla <s163920@student.dtu.dk>
2018-04-26 12:34:42 +0200

    Ordentlige værdier i ign og inj array

--- ECU2018/ECU2018/sources/ingAndinjArray.cpp
+++ ECU2018/ECU2018/sources/ingAndinjArray.cpp
@@ -6,10 +6,10 @@
 
 
 uint32_t ignitionArray[25] = {
-	0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 //sample values, should proberly be something else
+	20,	20,	20,	20,	20,	20,	20,	25,	25,	25,	30,	30,	30,	30,	35,	35,	35,	35,	35,	40,	40,	40,	40,	40,	45 //sample values, should proberly be something else
 };
 uint32_t injectionArray[25] = {
-	10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000 //sample values, choosen based on advanced calculations
+	6000, 4967, 5214, 6000, 6000, 4932, 6000, 4867, 4526, 4597, 4592, 4536, 4365, 4289, 4421, 4446, 4384,	4306, 4460, 4528, 4500,	4500, 4500, 4500, 4500//sample values, choosen based on advanced calculations
 };
 INTERPOL interpolation_map(double rpm) {
 	double RPM_DIVIDED = rpm / 250;
